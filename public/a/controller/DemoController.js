e2b9ca31a2d583c2a447e08c308bcd8fa8bb456a
Thomas McNamara <thomasmcnamara@vt.edu>
2023-09-07 02:26:54 +0000

    Controller now changes vectors instead of coords

--- public/a/controller/DemoController.js
+++ public/a/controller/DemoController.js
@@ -30,36 +30,37 @@ export default class DemoController extends Controller {
         // movement
         const gameBounds = this.getGame().getBounds();
         const speed = player.getSpeed() / refreshRate;
-        const unitVectors = {
-            horizontal: loggers.get("right").getKeyDown() + (-1 * loggers.get("left").getKeyDown()),
-            vertical: loggers.get("down").getKeyDown() + (-1 * loggers.get("up").getKeyDown())
-        }
-        if (unitVectors.horizontal != 0 || unitVectors.vertical != 0) {
-            if (unitVectors.vertical == 0) {
-                player.setXCoord(player.getXCoord() + (10 * speed * unitVectors.horizontal));
-            } else if (unitVectors.horizontal == 0) {
-                player.setYCoord(player.getYCoord() + (10 * speed * unitVectors.vertical));
-            } else {
-                player.setXCoord(player.getXCoord() + (7 * speed * unitVectors.horizontal));
-                player.setYCoord(player.getYCoord() + (7 * speed * unitVectors.vertical));
-            }
+        const unitVectors = [
+            loggers.get("right").getKeyDown() + (-1 * loggers.get("left").getKeyDown()),
+            loggers.get("down").getKeyDown() + (-1 * loggers.get("up").getKeyDown())
+        ];
 
-            // boundary collision
-            if (player.getXCoord() < 0) {
-                player.setXCoord(0);
-            }
-            if (player.getXCoord() + player.getSize() > this.getGame().getWidth()) {
-                player.setXCoord(this.getGame().getWidth() - player.getSize());
-            }
-            if (player.getYCoord() < 0) {
-                player.setYCoord(0);
-            }
-            if (player.getYCoord() + player.getSize() > this.getGame().getWidth()) {
-                player.setYCoord(this.getGame().getWidth() - player.getSize());
-            }
-
-            this.getConnection().emitMoved();
+        if (unitVectors[0] != 0 && unitVectors[1] != 0) {
+            unitVectors[0] *= speed * 7;
+            unitVectors[1] *= speed * 7;
+        } else {
+            unitVectors[0] *= speed * 10;
+            unitVectors[1] *= speed * 10;
         }
+        player.setVectors(unitVectors);
+
+        // boundary collision
+        // this needs to be moved into 
+        if (player.getXCoord() < 0) {
+            player.setXCoord(0);
+        }
+        if (player.getXCoord() + player.getSize() > this.getGame().getWidth()) {
+            player.setXCoord(this.getGame().getWidth() - player.getSize());
+        }
+        if (player.getYCoord() < 0) {
+            player.setYCoord(0);
+        }
+        if (player.getYCoord() + player.getSize() > this.getGame().getWidth()) {
+            player.setYCoord(this.getGame().getWidth() - player.getSize());
+        }
+
+        // this.getConnection().emitMoved();
+
         // size
         if (loggers.get("sizeUp").getKeyDown()) {
             player.grow(2);
